b96fcddea57d2fbef6dadbb917e454c1cfeeb711
J.C. Subida <jc.subida@northwestern.edu>
2024-03-09 11:01:48 -0600

    Fix SwiftMarkdown package

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "SteamPress",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v12)
     ],
     products: [
         .library(name: "SteamPress", targets: ["SteamPress"]),
@@ -14,14 +14,14 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.0.0"),
         .package(url: "https://github.com/vapor/leaf-kit.git", from: "1.0.0-rc.1"),
-        .package(name: "SwiftMarkdown", url: "https://github.com/vapor-community/markdown.git", from: "0.6.1"),
+        .package(url: "https://github.com/vapor-community/markdown.git", from: "0.6.1"),
     ],
     targets: [
         .target(name: "SteamPress", dependencies: [
             .product(name: "Vapor", package: "vapor"),
             .product(name: "LeafKit", package: "leaf-kit"),
             "SwiftSoup",
-            "SwiftMarkdown"
+            .product(name: "SwiftMarkdown", package: "markdown")
         ]),
         .testTarget(name: "SteamPressTests", dependencies: ["SteamPress"]),
     ]
